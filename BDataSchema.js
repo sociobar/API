af7aaa7a4671049e288ccb873957cd9fbcec2c26
Sociobae <144606948+sociobar@users.noreply.github.com>
2023-09-21 01:11:46 +0530

    Add files via upload

---
+++ BDataSchema.js
@@ -0,0 +1,49 @@
+export const BDataSchema = {
+    BusinessName: '',
+    BusinessEmail: {
+        Previous: '',
+        Platform: '',
+    },
+    BusinessPhone: {
+        Previous: '',
+        Platform: '',
+    },
+    BusinessAddress: {
+        Code: '',
+        Country: '',
+        State: '',
+        City: '',
+        Landmark: '',
+        Locality: '',
+        Street: '',
+        Address1: '',
+    },
+    BusinessRegistration: {
+        ROC: '',
+        CIN: '',
+        GST: '',
+        PAN: '',
+        RegistrationType: '',
+        SectorSize: '',
+    },
+    BusinessContact: {
+        ContactName: "",
+        ContactEmail: "",
+        ContactNumber: ""
+    },
+    BusinessLogo: {
+        LogoImage: "",
+        BannerImage: "",
+    },
+    BusinessPassword: "",
+    BusinessDescription: "",
+    BusinessWebsite: "",
+    BusinessFinancials: {
+        BankName: "",
+        IFSC_Code: "",
+        AccountNumber: "",
+        WalletNumber: "",
+        WalletAddress: "",    
+        WalletNumberCode: "",
+    }
+}
